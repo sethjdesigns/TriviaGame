02bf7716839e73a708e5459399f32f1ed6fc5de0
Seth Johnson <sethjdesigns@gmail.com>
2015-04-09 14:19:10 -0400

    changed output to JSON created viewmodel

--- TriviaGame/Scripts/_references.js
+++ TriviaGame/Scripts/_references.js
@@ -3,3 +3,7 @@
 /// <reference path="jquery-1.10.2.js" />
 /// <reference path="bootstrap.js" />
 /// <reference path="respond.js" />
+/// <reference path="jquery.validate.js" />
+/// <reference path="jquery.validate.unobtrusive.js" />
+/// <reference path="knockout-3.3.0.js" />
+/// <reference path="app.js" />
---
+++ TriviaGame/Scripts/app.js
@@ -0,0 +1,32 @@
+﻿var ViewModel = function () {
+
+    var self = this;
+    self.questions = ko.observableArray();
+    self.error = ko.observable();
+
+    var questionsUri = '/api/questions/';
+
+    function ajaxHelper(uri, method, data) {
+        self.error(''); // Clear error message
+        return $.ajax({
+            type: method,
+            url: uri,
+            dataType: 'json',
+            contentType: 'application/json',
+            data: data ? JSON.stringify(data) : null
+        }).fail(function (jqXHR, textStatus, errorThrown) {
+            self.error(errorThrown);
+        });
+    }
+
+    function getAllQuestions() {
+        ajaxHelper(questionsUri, 'GET').done(function (data) {
+            self.questions(data);
+        });
+    }
+
+    getAllQuestions();
+
+};
+
+ko.applyBindings(new ViewModel());
